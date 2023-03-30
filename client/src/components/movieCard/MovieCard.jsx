34c8ec40216f6de3d4f75fcdf18058fb6286417f
chtiwa <chtiwaa@gmail.com>
2023-03-30 02:03:26 +0100

    added more complex distance calc

---
+++ client/src/components/movieCard/MovieCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './movieCard.css'
+
+const MovieCard = () => {
+  return (
+    <div>MovieCard</div>
+  )
+}
+
+export default MovieCard
--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,18 +1,20 @@
-const Movie = require('./models/Movie')
-const asyncWrapper = require('./middleware/async-wrapper')
-const BaseError = require('./middleware/error-handler')
+const Movie = require('../models/Movie')
+const asyncWrapper = require('../middleware/async-wrapper')
+const BaseError = require('../middleware/error-handler')
 
 
 function calculateDistance(movie1, movie2) {
   // due to weird data by kaggle
-  // redo backend names and movie schema
-  const movie1Genres = JSON.parse(movie1.genres.replace(/'/g, '"')).map((genre) => genre.name)
-  const movie2Genres = JSON.parse(movie2.genres.replace(/'/g, '"')).map((genre) => genre.name)
+  const movie1Genres = movie1.Genre.split(',').map((genre) => genre)
+  const movie2Genres = movie2.Genre.split(',').map((genre) => genre)
 
   const union = [...movie1Genres, ...movie2Genres]
   const intersection = movie1Genres.filter((genre) => movie2Genres.includes(genre))
 
-  const distance = Math.sqrt(union.map((genre) => intersection.includes(genre) ? Math.pow(Number(movie1.vote_average) - Number(movie2.vote_average)) : 0).reduce((sum, x) => sum + x, 0))
+  const calcGenre = union.map((genre) => intersection.includes(genre) ? Math.pow(Number(movie1.vote_average) - Number(movie2.vote_average), 2) : 0).reduce((sum, x) => sum + x, 0)
+  const calcIMDB_Rating = Math.pow(Number(movie1.IMDB_Rating) - Number(movie1.IMDB_Rating), 2)
+  const calcMeta_score = Math.pow(Number(movie1.Meta_score) - Number(movie1.Meta_score), 2)
+  const distance = Math.sqrt(calcGenre + calcIMDB_Rating + calcMeta_score)
   return 1 / (1 + distance)
 }
 
@@ -22,7 +24,16 @@ const getMovieRecommendationV_1 = asyncWrapper(async (req, res) => {
     throw new BaseError("The movie doesn't exist", 404)
   }
 
-  const movies = await Movie.find({ _id: { $ne: movie._id } }).limit(100)
+  const movies = await Movie.find({ _id: { $ne: movie._id } }, {
+    Poster_Link: 1,
+    Series_Title: 1,
+    Released_Year: 1,
+    Runtime: 1,
+    Genre: 1,
+    IMDB_Rating: 1,
+    Meta_score: 1,
+    _id: 1
+  }).limit(50)
   const distances = movies.map((otherMovie) => ({ movie: otherMovie, distance: calculateDistance(movie, otherMovie) }))
 
   distances.sort((a, b) => b.distance - a.distance)
@@ -38,25 +49,26 @@ const getMovieRecommendationV_2 = asyncWrapper(async (req, res) => {
       $match: {
         // "vote_average": { $not: { $type: "string" } }, 
         // "vote_count": { $not: { $type: "string" } },
-        // "genres.name": req.query.genres, genres type is false when the  data was uploaded
-        "_id": { $ne: req.query._id }
+        // kaggle has got weird genre "Action, Drama" so the filtering based on the genre is off
+        // "Genre": req.query.Genre, 
+        "_id": { $ne: req.query.movie_id }
       }
     },
     {
       $project: {
-        original_title: 1,
-        vote_average: 1,
-        vote_count: 1,
-        overview: 1,
-        release_date: 1,
-        genres: 1,
-        runtime: 1,
+        Poster_Link: 1,
+        Series_Title: 1,
+        Released_Year: 1,
+        Runtime: 1,
+        Genre: 1,
+        IMDB_Rating: 1,
+        Meta_score: 1,
+        _id: 1,
         distance: {
           $sqrt: {
             $add: [
-              { $pow: [{ $subtract: [Number(req.query.vote_average), { $toDecimal: "$vote_average" }] }, 2] },
-              { $pow: [{ $subtract: [Number(req.query.vote_count), { $toDecimal: "$vote_count" }] }, 2] },
-              { $pow: [{ $subtract: [Number(req.query.runtime), { $toDecimal: "$runtime" }] }, 2] },
+              { $pow: [{ $subtract: [Number(req.query.IMDB_Rating), { $toDecimal: "$IMDB_Rating" }] }, 2] },
+              { $pow: [{ $subtract: [Number(req.query.Meta_score), { $toDecimal: "$Meta_score" }] }, 2] }
             ]
           }
         }
@@ -68,7 +80,7 @@ const getMovieRecommendationV_2 = asyncWrapper(async (req, res) => {
     }, {
       $sort: { distance: 1 }
     }, {
-      $limit: 10
+      $limit: 20
     }
   ])
 
@@ -76,24 +88,22 @@ const getMovieRecommendationV_2 = asyncWrapper(async (req, res) => {
 })
 
 const getMoviesByPage = asyncWrapper(async (req, res) => {
-  const limit = parseInt(req.query.limit) || 10
+  const limit = parseInt(req.query.limit) || 20
   const skip = limit * (parseInt(req.query.page || 1))
+  const totalPages = await Movie.countDocuments() / limit
   const movies = await Movie.find({}, {
-    fields: {
-      original_title: 1,
-      vote_average: 1,
-      vote_count: 1,
-      overview: 1,
-      release_date: 1,
-      genres: 1,
-      runtime: 1
-    },
-    limit: limit,
-    skip: skip,
-    sort: { _id: 1 }
-  })
+    Poster_Link: 1,
+    Series_Title: 1,
+    Released_Year: 1,
+    Runtime: 1,
+    Genre: 1,
+    IMDB_Rating: 1,
+    Meta_score: 1,
+    _id: 1
+  }).skip(skip).limit(limit).sort({ _id: -1 })
 
-  res.status(200).json({ success: true, movies: movies })
+
+  res.status(200).json({ success: true, movies: movies, totalPages: totalPages, page: req.query.page })
 })
 
 
